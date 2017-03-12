690b33760bff94086a9e558d54021061624342e6
CodeDead <admin@codedead.com>
2017-03-12 19:59:12 +0100

    * Added complete about window

---
+++ DeadLock/Windows/AboutWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using DeadLock.Classes;
+
+namespace DeadLock.Windows
+{
+    /// <summary>
+    /// Interaction logic for AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            LoadTheme();
+        }
+
+        private void LoadTheme()
+        {
+            StyleManager.ChangeStyle(this);
+        }
+
+        private void BtnClose_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void BtnLicense_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(AppDomain.CurrentDomain.BaseDirectory + "\\gpl.pdf");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "DeadLock", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnCodeDead_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start("http://codedead.com/");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "DeadLock", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
